3a3564d567d90eec133383d15fdced2f45381e36
Asko Hulkko <hulkko54@gmail.com>
2023-10-16 20:07:57 +0300

    Created project basic view

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+// use ejs files to prepare templates for views
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res) => {
-	res.send('test')
+	// task list data
+	const tasks = ['Study HTML', 'Study CSS', 'Study JS', 'Study OOP']
+	res.render('index', {tasks: tasks})
 })
 
 
