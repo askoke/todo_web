9912ac17265dcd3259f08a50d45333c8b3385253
Asko Hulkko <hulkko54@gmail.com>
2023-10-25 16:11:03 +0300

    Delete single task data from file

--- index.js
+++ index.js
@@ -7,20 +7,89 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const readFile = (filename) => {
+	return new Promise((resolve, reject) => {
+		// get data from file
+		fs.readFile(filename, 'utf8', (err, data) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			// tasks list data from file
+			const tasks = JSON.parse(data)
+			resolve(tasks)
+		});
+	})
+}
+
+const writeFile = (filename, data) => {
+	return new Promise((resolve, reject) => {
+		//get data from file
+		fs.writeFile(filename, data, 'utf-8', err => {
+			if(err){
+				console.error(err);
+				return;
+			}
+			resolve(true)
+		});
+	})
+}
+
 app.get('/', (req, res) => {
 	// get data from file
-	fs.readFile('./tasks', 'utf8', (err, data) => {
-		if(err){
-			console.error(err);
-			return;
-		}
-		//task list data
-		const tasks = data.split('\n')
+	readFile('./tasks.json')
+	.then(tasks => {
+		console.log(tasks)
 		res.render('index', {tasks: tasks})
 	})
-	
 })
 
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+	// tasks list data from file
+	readFile('./tasks.json')
+	.then(tasks => {
+		// add new task
+		//create new id automatically
+		let index
+		if(tasks.length === 0){
+			index = 0
+		} else {
+			index = tasks[tasks.length-1].id + 1;
+		}
+		// create task object
+		const newTask = {
+			"id" : index,
+			"task" : req.body.task
+		}
+		console.log(newTask)
+		//add form sent task to tasks array
+		tasks.push(newTask)
+		console.log(tasks)
+		data = JSON.stringify(tasks, null, 2)
+		writeFile('./tasks.json', data)
+		// redirect to / to see result
+		res.redirect('/')
+	})
+})
+
+app.get('/delete-task/:taskId', (req, res) => {
+	let deleteTaskId = parseInt(req.params.taskId)
+	readFile('./tasks.json')
+	.then(tasks => {
+		tasks.forEach((task, index) => {
+			if(task.id === deleteTaskId){
+				tasks.splice(index, 1)
+			}
+		})
+		data = JSON.stringify(tasks, null, 2)
+		writeFile('./tasks.json', data)
+			// redirect to / to see result
+			re.redirect('/')
+	})
+})
 
 app.listen(3001, () => {
 	console.log('Example app is started at https://localhost:3001')
